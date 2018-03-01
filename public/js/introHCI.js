3d6defe72fb3b3b13fb476eb69b5fdb387f07cde
Minghan(Kelsey) Guo <mig037@ucsd.edu>
2018-03-01 10:49:52 -0800

    Kelsey's commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -15,12 +15,32 @@ function initializePage() {
  	initRSVPForm();
 }
 
+
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold",tapholdHandler);
+
+		function tapholdHandler(event){
+			//get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
 }
+
+//register swipe event listener
+$(function(){
+	$("div.box").bind("taphold",tapholdHandler);
+	function tapholdHandler(event){
+		$(event.target).addClass("taphold");
+	}
+});
